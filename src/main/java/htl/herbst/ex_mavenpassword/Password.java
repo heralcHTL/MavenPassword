cb8d0d69c22688a0ad6f58a889df6fcc01fb2157
heralc15\Alex <rednax.herbst@gmail.com>
2019-03-22 12:17:25 +0100

    Simple name change

--- src/main/java/htl/herbst/ex_mavenpassword/Password.java
+++ src/main/java/htl/herbst/ex_mavenpassword/Password.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package htl.herbst.ex_mavenproducerconsumer;
+package htl.herbst.ex_mavenpassword;
 
 /**
  *
